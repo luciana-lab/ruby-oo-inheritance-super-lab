1e308815c432cb3badbf41c7820165f21d4ba069
Luciana Sugeta <lulusugeta@gmail.com>
2021-03-23 22:25:03 -0400

    commit tree 9004af67f6301eae5c06d6b4cb0c169b871806d4

---
+++ lib/student.rb
@@ -0,0 +1,13 @@
+class Student
+    
+    #returns a greeting phrase
+    def hello
+        puts "Hey there! I'm so excited to learn stuff."
+    end
+
+    #returns the phrase 'Pick me!'
+    def raise_hand
+        puts "Pick me!"
+    end
+
+end
